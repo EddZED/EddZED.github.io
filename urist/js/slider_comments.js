94710568173c9274bd03cc9109ab2ecf8b953706
EddZED <Eddie_NT@mail.ru>
2020-07-03 08:53:04 +0500

    ass

---
+++ urist/js/slider_comments.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $('.slider_comments-block').slick({
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
