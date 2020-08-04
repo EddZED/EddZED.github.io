bf5ebe1a55ade00b9c3e16272f8d302a2d76fb5c
EddZED <Eddie_NT@mail.ru>
2020-08-05 03:07:42 +0500

    фввв

---
+++ watt/js/card_slider.js
@@ -0,0 +1,22 @@
+$('.view_window-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  infinite: true,
+  asNavFor: '.min_card-slider',
+  prevArrow: '<button type = "button" class = "slick-prev"><img class="img-fluid" src="img/ar_left-slider.png"></ button>',
+  nextArrow: '<button type = "button" class = "slick-next"><img class="img-fluid" src="img/ar_right-slider.png"></ button>',
+  appendArrows: $('.view_window-slider')
+});
+$('.min_card-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.view_window-slider',
+  dots: false,
+  infinite: true,
+  centerMode: true,
+  centerPadding: '0',
+  focusOnSelect: true,
+  slickRemove: 'index'
+});
