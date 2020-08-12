dd5bad5cea68c902a6abd05fd41922251598a6f3
EddZED <Eddie_NT@mail.ru>
2020-08-12 18:13:01 +0500

    фвв

--- astima/js/answer_slider.js
+++ astima/js/answer_slider.js
@@ -2,5 +2,8 @@ $('.slider_test').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: false,
-  arrow: true
+  arrow: true,
+  appendArrows: $('.nav_slider'),
+  nextArrow: '<button type="button" class="btn_header yellow_btn btn_slide">Следующий вопрос</button>',
+  adaptiveHeight: true
 })
