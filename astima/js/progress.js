efbe575bc8574cf2af0008befc4ece71aa95f9fc
EddZED <Eddie_NT@mail.ru>
2020-08-13 04:09:16 +0500

    asdd

--- astima/js/progress.js
+++ astima/js/progress.js
@@ -1,6 +1,8 @@
 $('.slider_test').on('afterChange', function(event, slick, currentSlide){
-
-  $('.progress__count').text(currentSlide + 1);
-  $('.progress__performed').width( 20 * (currentSlide + 1) + '%' );
-
+  $('.progress-bar').width( `${20 * (currentSlide + 1)}%` );
+});
+$(".btn_slide").click(function() {
+  var $price = $(".index_per");
+  $price.val(parseInt($price.val()) + 20 );
+  $price.change();
 });
