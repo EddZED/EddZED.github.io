f5432c30ccc4141c43af97622f487c801bba425d
EddZED <Eddie_NT@mail.ru>
2020-08-30 05:04:00 +0500

    add

---
+++ volyer/js/slider_comments.js
@@ -0,0 +1,9 @@
+$('.comments').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: true,
+  initialSlide: 1,
+  appendArrows: $('.slider_nav'),
+  appendDots: $('.slider_nav')
+})
