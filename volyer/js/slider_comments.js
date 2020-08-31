467e138e21249556206f9aa213ca324a30ef9cfb
EddZED <Eddie_NT@mail.ru>
2020-08-31 13:26:53 +0500

    asdsad

--- volyer/js/slider_comments.js
+++ volyer/js/slider_comments.js
@@ -4,6 +4,9 @@ $('.comments').slick({
   dots: true,
   arrows: true,
   initialSlide: 1,
+  adaptiveHeight: true,
   appendArrows: $('.slider_nav'),
-  appendDots: $('.slider_nav')
+  appendDots: $('.slider_nav'),
+  prevArrow: '<button type = "button" class = "slick-prev"><img class="img-fluid" src="img/ar_left.png"></ button>',
+  nextArrow: '<button type = "button" class = "slick-next"><img class="img-fluid" src="img/ar_right.png"></ button>'
 })
