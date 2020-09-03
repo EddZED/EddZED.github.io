31e2f3186d41fac6958229a612d42f2df061b11f
EddZED <Eddie_NT@mail.ru>
2020-09-04 00:48:00 +0500

    sd

--- kazproject/js/rezults_sl.js
+++ kazproject/js/rezults_sl.js
@@ -2,7 +2,10 @@ $('.slider_body').slick({
   sliderToShiw: 1,
   sliderToScroll: 1,
   infinite: false,
-  fade: true
+  fade: true,
+  appendArrows: $('.nav_slider'),
+  prevArrow: '<button type = "button" class = "slick-prev">Смотреть предидущий отзыв</ button>',
+  nextArrow: '<button type = "button" class = "slick-next">Смотреть следующий отзыв</ button>'
 })
 
 
