d502d9f64f599678ceaf7f7a56e656122028df56
EddZED <Eddie_NT@mail.ru>
2020-09-13 21:27:59 +0500

    asd

---
+++ remonttv/js/slider_rezults.js
@@ -0,0 +1,12 @@
+$('.slider_rezults').slick({
+  centerMode: true,
+  centerPadding: '0',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arows: true,
+  infinite: false,
+  variableWidth: true,
+  initialSlide: 1,
+  nextArrow: '<button type = "button" class = "slick-next"><img src="img/arr_right.png" class="img-fluid"></ button>',
+  prevArrow: '<button type = "button" class = "slick-prev"><img src="img/arr_left.png" class="img-fluid"></ button>',
+});
