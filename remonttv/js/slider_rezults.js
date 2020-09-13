2d60c0d8aa34e5e63ff2e7f9e0bed8a18bad327d
EddZED <Eddie_NT@mail.ru>
2020-09-13 22:57:01 +0500

    asd

--- remonttv/js/slider_rezults.js
+++ remonttv/js/slider_rezults.js
@@ -7,6 +7,25 @@ $('.slider_rezults').slick({
   infinite: false,
   variableWidth: true,
   initialSlide: 1,
-  nextArrow: '<button type = "button" class = "slick-next"><img src="img/arr_right.png" class="img-fluid"></ button>',
-  prevArrow: '<button type = "button" class = "slick-prev"><img src="img/arr_left.png" class="img-fluid"></ button>',
+  nextArrow: '<button type = "button" class = "slick-next d-none d-xl-block"><img src="img/arr_right.png" class="img-fluid"></ button>',
+  prevArrow: '<button type = "button" class = "slick-prev d-none d-xl-block"><img src="img/arr_left.png" class="img-fluid"></ button>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        appendArrows: $('nav_sl')
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+      }
+    }
+  ]
 });
+$('.slick-prev-2').click(function(event){
+  $('.slider_rezults').slick('slickPrev');
+});
+$('.slick-next-2').click(function (event) {
+  $('.slider_rezults').slick('slickNext');
+})
