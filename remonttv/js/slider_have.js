675c4f35bca1004693108d24c9e3058582006bf3
EddZED <Eddie_NT@mail.ru>
2020-09-14 13:13:26 +0500

    asd

--- remonttv/js/slider_have.js
+++ remonttv/js/slider_have.js
@@ -1,11 +1,18 @@
   $('.slider_have').slick({
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    responsive: [{
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }]
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      rows: 2,
+      infinite: false,
+      variableWidth: true
+  });
+  $('.slider_have').slick('setPosition');
+  $('.mobile_slider-have').slick({
+    centerMode:true,
+    centerPadding: '0',
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows: $('.nav_sl-mobile'),
+    nextArrow: '<button type = "button" class = "slick-next"><img src="img/right_2.png" class="img-fluid"></ button>',
+    prevArrow: '<button type = "button" class = "slick-prev"><img src="img/white_left_2.png" class="img-fluid"></ button>',
   })
