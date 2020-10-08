95652f0cbd67bb9b33079ee1b8b784f7a5cfba60
EddZED <Eddie_NT@mail.ru>
2020-10-08 22:50:17 +0500

    add

---
+++ lesson/js/script.js
@@ -0,0 +1,9 @@
+console.log('35' + - '22');
+console.log('35' * '22');
+console.log('558' > 22);
+let user = 0;
+let newUser = user++;
+console.log(newUser)
+console.log(!false && 11 || 18 && !'');
+let name = 0;
+console.log(name ?? 'Без имени')
