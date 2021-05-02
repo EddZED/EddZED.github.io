c6eee72a8e282d901ff76b10674db7df67946e5d
EddZED <Eddie_NT@mail.ru>
2021-05-02 17:48:35 +0500

    add

--- al_okna/js/slider_work.js
+++ al_okna/js/slider_work.js
@@ -9,26 +9,27 @@ $('.slider_works').slick({
       variableWidth: true,
       initialSlide: 1,
       centerMode: true,
+      centerPadding: '33px',
       //appendArrows: $('.slider_nav'),
       nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><img src="img/sl_right.png" class="img-fluid"></button>',
-      prevArrow: '<button class="slick-prev slick-arrow" aria-label="Prev" type="button" style=""><img src="img/sl_left.png" class="img-fluid"></button>'
-      //responsive: [
-      //  {
-      //    breakpoint: 1199,
-      //    settings: {
-      //      slidesToShow: 3,
-      //      sldiesPerRow: 1,
-      //      rows: 1
-      //    }
-      //  },
-      //  {
-      //    breakpoint: 991,
-      //    settings: {
-      //      slidesToShow: 2,
-      //      sldiesPerRow: 2,
-      //      rows: 2
-      //    }
-      //  }
-      // ]
+      prevArrow: '<button class="slick-prev slick-arrow" aria-label="Prev" type="button" style=""><img src="img/sl_left.png" class="img-fluid"></button>',
+      responsive: [
+        {
+          breakpoint: 320,
+         settings: {
+           slidesToShow: 1,
+           sldiesPerRow: 1,
+           variableWidth: true
+          }
+        },
+        {
+          breakpoint: 767,
+         settings: {
+           slidesToShow: 1,
+           sldiesPerRow: 1,
+           variableWidth: false
+          }
+        }
+       ]
     });
     $('.slider_works').slick('setPosition');
