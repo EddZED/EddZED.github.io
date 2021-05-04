48847d75a17af0356f8a78163a44c993561dfe65
EddZED <Eddie_NT@mail.ru>
2021-05-04 06:13:14 +0500

    add

---
+++ al_okna/js/slider_options.js
@@ -0,0 +1,33 @@
+ $('.slider_wrapper_two').slick({
+      slidesToShow: 4,
+      sldiesPerRow: 1,
+      rows: 1,
+      autoplay: false,
+      arrows: true,
+      infinite: false,
+      dots: false,
+      variableWidth: false,
+      centerMode: false,
+      appendArrows: $('.slider_nav'),
+      nextArrow: '<button class="button-next"><img src="img/right_ar.png" class="img-fluid"></button>',
+      prevArrow: '<button class="button-prev"><img src="img/left_ar.png" class="img-fluid"></button>',
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+            sldiesPerRow: 1,
+            rows: 1
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            sldiesPerRow: 2,
+            rows: 2
+          }
+        }
+      ]
+    });
+    $('.slider_wrapper_two').slick('setPosition');
