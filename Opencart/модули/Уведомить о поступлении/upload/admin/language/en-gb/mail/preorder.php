24d6b877e7dc1f71925a55236211b797a77088b8
EddZED <Eddie_NT@mail.ru>
2021-06-17 06:43:49 +0500

    remove opencart

--- Opencart/модули/Уведомить о поступлении/upload/admin/language/en-gb/mail/preorder.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Text
-$_['text_subject']	= 'Product %s in Stock';
-$_['text_header']	= 'Product %s in Stock.';
-$_['text_link']		= 'Product:';
-$_['text_option']	= 'Product options:';
-
-
-
