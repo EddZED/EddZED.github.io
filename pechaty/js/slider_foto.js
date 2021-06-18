f7dcba81a791e79031c09328b51c859cbc5b4e63
EddZED <Eddie_NT@mail.ru>
2021-06-18 17:41:22 +0500

    add

---
+++ pechaty/js/slider_foto.js
@@ -0,0 +1,27 @@
+$('.slider_foto_block').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        appendArrows: $('.arrows_foto'),
+        prevArrow: '<button type = "button" class = "slick-prev"><img class="img-fluid" src="img/arrow_left.svg"></ button>',
+        nextArrow: '<button type = "button" class = "slick-next"><img class="img-fluid" src="img/arrow_right.svg"></ button>',
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
