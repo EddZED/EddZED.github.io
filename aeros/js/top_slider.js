62b50e2c5a69efb87e484d5b0792d9be2910e8ff
EddZED <Eddie_NT@mail.ru>
2021-06-24 18:24:03 +0500

    add

--- aeros/js/top_slider.js
+++ aeros/js/top_slider.js
@@ -12,5 +12,41 @@ $('.intro_slider_nav').slick({
   dots: false,
   centerMode: true,
   centerPadding: '0px',
-  focusOnSelect: true
+  focusOnSelect: true,
+  prevArrow: '<button type = "button" class = "slick-prev"><img class="img-fluid" src="img/sl_left.png" alt="передъидущий слайд"></ button>',
+  nextArrow: '<button type = "button" class = "slick-next"><img class="img-fluid" src="img/sl_right.png" alt="следующий слайд"></ button>',
+  responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: false,
+          centerMode: false,
+          arrows: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 565,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '0px',
+          arrows: true,
+          adaptiveHeight: false,
+          dots: true
+         }
+      }
+    ]
 });
