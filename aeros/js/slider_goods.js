50eb6a7b7b4ed597826b2abc4c3d6637ef38a4a3
EddZED <Eddie_NT@mail.ru>
2021-06-27 17:08:19 +0500

    add

--- aeros/js/slider_goods.js
+++ aeros/js/slider_goods.js
@@ -1,38 +1,42 @@
 $('.slider_cards_goods').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
-  TouchMove: true,
+  TouchMove: false,
   arrows: true,
-  draggable: true,
+  draggable: false,
   fade: false,
-  focusOnSelect: true,
+  infinite: true,
+  focusOnSelect: false,
+  prevArrow: '<button type = "button" class = "slick-prev"><img class="img-fluid" src="svg/left_goods.svg" alt="передъидущий слайд"></ button>',
+  nextArrow: '<button type = "button" class = "slick-next"><img class="img-fluid" src="svg/right_goods.svg" alt="следующий слайд"></ button>',
   responsive: [
       {
-        breakpoint: 991,
+        breakpoint: 1199,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          infinite: false,
+          centerMode: false,
+          draggable: false,
+          arrows: true
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
           centerMode: false,
           arrows: true
         }
       },
       {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: true
-        }
-      },
-      {
-        breakpoint: 565,
+        breakpoint: 374,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          arrows: true,
-          adaptiveHeight: false
-         }
+          centerMode: false,
+          arrows: true
+        }
       }
     ]
 });
