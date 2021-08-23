26e3b25241d059de0ba88af027d236316cc7c3b4
EddZED <Eddie_NT@mail.ru>
2021-08-23 23:16:51 +0500

    add

---
+++ aeros/js/slider_category.js
@@ -0,0 +1,12 @@
+$('.slider_good_category').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  TouchMove: true,
+  arrows: false,
+  infinite: false,
+  dots: true,
+  draggable: true,
+  fade: false,
+  focusOnSelect: true
+  });
+$('.slider_good_category').slick('setPosition');
