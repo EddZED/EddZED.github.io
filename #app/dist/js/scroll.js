b90b974af0c575faad0013c5ba06aa732e730de6
EddZED <Eddie_NT@mail.ru>
2021-09-09 16:11:34 +0500

    add

--- #app/dist/js/scroll.js
+++ #app/dist/js/scroll.js
@@ -56,7 +56,48 @@ $(document).ready(function() {
    });
 	} else {
 		$(function () {
-			
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 3000, offset: 0, triggerHook: 0})
+				.setPin("#thirdSection")	
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 5000, offset: 0, triggerHook: 0})
+				.setClassToggle("#cont1", "show")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 800, offset: 0, triggerHook: 0})
+				.setClassToggle("#process-design", "animate")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 5000, offset: 800, triggerHook: 0})
+				.setClassToggle("#cont2", "show")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 800, offset: 800, triggerHook: 0})
+				.setClassToggle("#process-phone", "animate")
+				.addTo(controller);	
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 5000, offset: 1600, triggerHook: 0})
+				.setClassToggle("#cont3", "show")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 800, offset: 1600, triggerHook: 0})
+				.setClassToggle("#process-phone", "animate2")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 5000, offset: 2400, triggerHook: 0})
+				.setClassToggle("#cont4", "show")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 8000, offset: 2400, triggerHook: 0})
+				.setClassToggle("#process-phone", "animate3")
+				.addTo(controller);
+			// new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 380, offset: 420, triggerHook: 0})
+			// 	.setClassToggle("#process-phone", "unanimate")
+			// 	.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 800, offset: 3200, triggerHook: 0})
+				.setClassToggle("#process-marketing", "animate")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 5000, offset: 3200, triggerHook: 0})
+				.setClassToggle("#cont5", "show")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 5000, offset: 4000, triggerHook: 0})
+				.setClassToggle("#process-launch", "animate")
+				.addTo(controller);
+			new ScrollMagic.Scene({triggerElement: "#thirdSection", duration: 5000, offset: 4000, triggerHook: 0})
+				.setClassToggle("#cont6", "show")
+				.addTo(controller);
 		});	
 	};
 });
