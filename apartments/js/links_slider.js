923a4f7a61dd193b45e5a128a6381c077489d6ba
EddZED <Eddie_NT@mail.ru>
2021-12-01 17:14:30 +0500

    add

---
+++ apartments/js/links_slider.js
@@ -0,0 +1,20 @@
+$('.links_item').click(function(){
+      $('.links_item').removeClass('active');
+      $('.slider_inner_img').removeClass('active');
+      $(this).addClass('active');
+    });
+    $('.item_one').click(function(){
+      $('.img_1').addClass('active');
+    });
+    $('.item_two').click(function(){
+      $('.img_2').addClass('active');
+    });
+    $('.item_three').click(function(){
+      $('.img_3').addClass('active');
+    });
+    $('.item_four').click(function(){
+      $('.img_4').addClass('active');
+    });
+    $('.item_five').click(function(){
+      $('.img_5').addClass('active');
+    });
