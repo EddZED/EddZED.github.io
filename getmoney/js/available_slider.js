aabcb7fd06fc030269cd5e47cc8f595f9afa0176
EddZED <51696670+EddZED@users.noreply.github.com>
2023-05-17 06:56:15 +0500

    adasd

---
+++ getmoney/js/available_slider.js
@@ -0,0 +1,6 @@
+ $('.slider_available-credits').slick({
+    fade: true,
+    appendArrows: ".arrows_slider",
+    prevArrow: '<button type = "button" class = "slick-prev"><svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.7137 4.21415H1.28516M1.28516 4.21415L4.71373 0.571289M1.28516 4.21415L4.71373 7.64272" stroke="#DCDCDC"/></svg></button>',
+      nextArrow: '<button type = "button" class = "slick-next"><svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.286272 4.21439H9.71484M9.71484 4.21439L6.28627 0.571533M9.71484 4.21439L6.28627 7.64296" stroke="#DCDCDC"/></svg></button>'
+ });
